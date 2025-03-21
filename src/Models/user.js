cc48507953a48ef450635363285ff1f8e551424a
piyushSharma97 <piyushsharma70141@gmail.com>
2025-03-21 11:41:37 +0530

    schema and module

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -17,20 +20,57 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+              throw new Error("Invalid Email :" + value);
+            }
+          },
       },
       password: {
         type: String,
         required: true,
-      
+        // validate(value) {
+        //     if (!validator.isStrongPassword(value)) {
+        //       throw new Error("Enter Strong password :" + value);
+        //     }
+        //   },
       },
       about: {
         type: String,
       },
-      skills: {
-        type: [String],
+    //   skills: {
+    //     type: [String],
+    //   },
+      gender: {
+        type: String,
+        required: false,
+        trim: true,
+        // validate(value) {
+        //   if (!["male", "female", "others","Male", "Female", "Others"].includes(value)) {
+        //     throw new Error("Not a valid gender (Male , Female and other)");
+        //   }
+        // },
       },
-},  {
+    //   photoURL: {
+    //     type: String,
+    //     default: "https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?t=st=1740779693~exp=1740783293~hmac=3ffc11733917c931bddeec957e8fa649e6a1590282b3210d816ccbf54dab2e94&w=900",
+    //     validate(value) {
+    //       if (!validator.isURL(value)) {
+    //         throw new Error("Invalid URL :" + value);
+    //       }
+    //     },
+    //   },
+      age: {
+        type: Number,
+        required: false,
+        min: 18,
+      },
+    },  
+    {
     timestamps: true,
-  })
+  }
+)
 
-  
+
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel;
--- src/app.js
+++ src/app.js
@@ -3,12 +3,26 @@ const connectDB = require('./Config/db')
 const dotenv = require("dotenv");
 const {userAuth} =require('./middleware/auth')
 const app = express();
+const User = require('./Models/user')
 dotenv.config({});
 const PORT = process.env.PORT
 
 app.use(express.json());
 
+app.post('/signup',(req,res)=>{
+ const userObj= {
+    firstName:'sadasd',
+    lastName:'sdfsd',
+    emailId:'piyushH.cp',
+    age:25,
+    gender:"male",
+    password:'2131233'
 
+ }
+ const user = new User(userObj);
+ user.save()
+ res.send('complete')
+})
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
